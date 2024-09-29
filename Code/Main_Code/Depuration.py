0fb77146b579a271f273754b1ba79eee1166a08c
Mamas <joaovpereira1411@gmail.com>
2024-09-29 20:57:21 -0300

    eu ainda amo minha ex

--- Code/Main_Code/Depuration.py
+++ Code/Main_Code/Depuration.py
@@ -13,30 +13,183 @@ for i in range(100):  # Alterar para gerar mais filhos, se necessário
         print(f"Filho Número: 0{Filho_Numero}")
     else:
         print(f"Filho Número: {Filho_Numero}")
-        
-    # Geração do zigoto do filho
+    
+    print("\n")
+
+# (Pele)
+
+    print("                [Pele]")   
+    # Geração do zigoto do filho   
     Zigoto_Filho = Zigoto_filho_String_depump(Zigoto_Mae_String, Zigoto_Pai_String)
-    print(f"Zigoto do filho: ({Zigoto_Filho[0]}{Zigoto_Filho[1]})")
+    print(f"          Zigoto:         {Zigoto_Filho[0]}{Zigoto_Filho[1]}")
     
     # Converte os genes em números e soma
     Zigoto_Filho_Valor = Zigoto_filho_Numb_depump(Zigoto_Filho)
-    print(f"Resultado da soma dos genes: {Zigoto_Filho_Valor}")
+    print(f"          Soma dos genes: {Zigoto_Filho_Valor}")
     
     # Verificação de homozigoto ou heterozigoto
     tipo_zigoto = verificar_zigoto(Zigoto_Filho)
-    print(f"Tipo de Zigoto: {tipo_zigoto}")
-    
+    print(f"          {tipo_zigoto}.")
+
+    AA, Aa_aA, aa = contagem(AA, Aa_aA, aa, Zigoto_Filho)
+
+    # Contagem de Hetero/Homo zigotos
     if tipo_zigoto == "Homozigoto": 
         Homozigoto += 1
     if tipo_zigoto == "Heterozigoto":
         Heterozigoto += 1
+    # Fim
+# Fim
+
+    print("\n")
+
+
+# (Olhos)
+    print("               [Olhos]")
     
+    # Geração do zigoto do filho 
+    Zigoto_Filho = Zigoto_filho_String_depump(Zigoto_Mae_String, Zigoto_Pai_String)
+    print(f"          Zigoto:         {Zigoto_Filho[0]}{Zigoto_Filho[1]}")
+    # Fim
     
+    # Converte os genes em números e soma
+    Zigoto_Filho_Valor = Zigoto_filho_Numb_depump(Zigoto_Filho)
+    print(f"          Soma dos genes: {Zigoto_Filho_Valor}")
+    # Fim
+    
+    # Verificação de homozigoto ou heterozigoto
+    tipo_zigoto = verificar_zigoto(Zigoto_Filho)
+    print(f"          {tipo_zigoto}.")
+    # Fim
+    
+    AA, Aa_aA, aa = contagem(AA, Aa_aA, aa, Zigoto_Filho)
+    
+    # Contagem de Hetero/Homo zigotos
+    if tipo_zigoto == "Homozigoto": 
+        Homozigoto += 1
+    if tipo_zigoto == "Heterozigoto":
+        Heterozigoto += 1
+    # Fim
+    
+# Fim
+
+    print("\n")
+# (Nariz)
+
+    print("               [Nariz]")   
+    
+    # Geração do zigoto do filho
+    Zigoto_Filho = Zigoto_filho_String_depump(Zigoto_Mae_String, Zigoto_Pai_String)
+    print(f"          Zigoto:         {Zigoto_Filho[0]}{Zigoto_Filho[1]}")
+    
+    # Converte os genes em números e soma
+    Zigoto_Filho_Valor = Zigoto_filho_Numb_depump(Zigoto_Filho)
+    print(f"          Soma dos genes: {Zigoto_Filho_Valor}")
+    
+    # Verificação de homozigoto ou heterozigoto
+    tipo_zigoto = verificar_zigoto(Zigoto_Filho)
+    print(f"          {tipo_zigoto}.")
+    
+    AA, Aa_aA, aa = contagem(AA, Aa_aA, aa, Zigoto_Filho)
+    
+    # Contagem de Hetero/Homo zigotos
+    if tipo_zigoto == "Homozigoto": 
+        Homozigoto += 1
+    if tipo_zigoto == "Heterozigoto":
+        Heterozigoto += 1
+    # Fim
+# Fim    
+
+    print("\n")
+# (Altura)
+
+    print("              [Altura]")   
+    
+    # Geração do zigoto do filho
+    Zigoto_Filho = Zigoto_filho_String_depump(Zigoto_Mae_String, Zigoto_Pai_String)
+    print(f"          Zigoto:         {Zigoto_Filho[0]}{Zigoto_Filho[1]}")
+    
+    # Converte os genes em números e soma
+    Zigoto_Filho_Valor = Zigoto_filho_Numb_depump(Zigoto_Filho)
+    print(f"          Soma dos genes: {Zigoto_Filho_Valor}")
+    
+    # Verificação de homozigoto ou heterozigoto
+    tipo_zigoto = verificar_zigoto(Zigoto_Filho)
+    print(f"          {tipo_zigoto}.")
+    
+    AA, Aa_aA, aa = contagem(AA, Aa_aA, aa, Zigoto_Filho)    
+    
+    # Contagem de Hetero/Homo zigotos
+    if tipo_zigoto == "Homozigoto": 
+        Homozigoto += 1
+    if tipo_zigoto == "Heterozigoto":
+        Heterozigoto += 1
+    # Fim
+# Fim
+
+    print("\n")
+
+    
+# (Tipo de cabelo)
+
+    print("           [Tipo de cabelo]")
+    # Geração do zigoto do filho
+    Zigoto_Filho = Zigoto_filho_String_depump(Zigoto_Mae_String, Zigoto_Pai_String)
+    print(f"          Zigoto:         {Zigoto_Filho[0]}{Zigoto_Filho[1]}")
+    
+    # Converte os genes em números e soma
+    Zigoto_Filho_Valor = Zigoto_filho_Numb_depump(Zigoto_Filho)
+    print(f"          Soma dos genes: {Zigoto_Filho_Valor}")
+    
+    # Verificação de homozigoto ou heterozigoto
+    tipo_zigoto = verificar_zigoto(Zigoto_Filho)
+    print(f"          {tipo_zigoto}.")
+    
+    AA, Aa_aA, aa = contagem(AA, Aa_aA, aa, Zigoto_Filho)    
+    
+    # Contagem de Hetero/Homo zigotos
+    if tipo_zigoto == "Homozigoto": 
+        Homozigoto += 1
+    if tipo_zigoto == "Heterozigoto":
+        Heterozigoto += 1
+    # Fim
+# Fim
+
+    print("\n")
+
+# (Cor de cabelo) 
+
+    print("           [Cor do cabelo]")  
+    # Geração do zigoto do filho
+    Zigoto_Filho = Zigoto_filho_String_depump(Zigoto_Mae_String, Zigoto_Pai_String)
+    print(f"          Zigoto:         {Zigoto_Filho[0]}{Zigoto_Filho[1]}")
+    
+    # Converte os genes em números e soma
+    Zigoto_Filho_Valor = Zigoto_filho_Numb_depump(Zigoto_Filho)
+    print(f"          Soma dos genes: {Zigoto_Filho_Valor}")
+    
+    # Verificação de homozigoto ou heterozigoto
+    tipo_zigoto = verificar_zigoto(Zigoto_Filho)
+    print(f"          {tipo_zigoto}.")
+
+    AA, Aa_aA, aa = contagem(AA, Aa_aA, aa, Zigoto_Filho)
+    
+    # Contagem de Hetero/Homo zigotos
+    if tipo_zigoto == "Homozigoto": 
+        Homozigoto += 1
+    if tipo_zigoto == "Heterozigoto":
+        Heterozigoto += 1
+    # Fim
+# Fim
+
+    print("\n")
+
+    print("[Fim do DNA do filho]")
     
     print("\n")
     
     # Contagem de combinações de zigotos
-    AA, Aa_aA, aa = contagem(AA, Aa_aA, aa, Zigoto_Filho)
+
     
 # Exibe os resultados finais
 print(f"AA: {AA}")
---
+++ Code/Main_Code/Olhos.py
@@ -0,0 +1,35 @@
+import Zigoto 
+import random
+
+class Gene_Olho:
+    def __init__(self, letra, valor):
+        self.letra = letra  # String
+        self.valor = valor  # 2 para dominante, 1 para recessivo
+
+    def __str__(self):
+        return self.letra
+
+    # Comparação com base no valor numérico
+    def __eq__(self, outro):
+        return self.valor == (outro.valor if isinstance(outro, Gene_Olho) else outro)
+
+# Criando genes dominante e recessivo
+Olhos_castanhos = Gene_Olho('Olhos castanhos', Zigoto.Dominante.valor) # Um gene simplesmente dominante
+Olhos_amarelos = Gene_Olho('Olhos castanhos', Zigoto.Dominante.valor) # Um gene dominante, porém depende da mesclagem de melanina (Intermediária)
+Olhos_verdes = Gene_Olho('Olhos verdes', Zigoto.Recessivo.valor) # Ele é recessivo, e é isso
+Olhos_azuis = Gene_Olho('Olhos azuis', Zigoto.Recessivo.valor) # Ele é recessivo, e é isso
+
+Zigoto_Olho_Mae = [Olhos_castanhos, Olhos_azuis]
+Zigoto_Olho_Pai = [Olhos_azuis, Olhos_azuis]
+
+Gene_Olho_Mae = random.choice(Zigoto_Olho_Mae)
+Gene_Olho_Pai = random.choice(Zigoto_Olho_Pai)
+
+Zigoto_Olho_Filho = [Gene_Olho_Mae, Gene_Olho_Pai]
+
+if Gene_Olho_Mae.valor + Gene_Olho_Pai.valor == 3:
+    Zigoto_Olho_Filho = Olhos_castanhos
+else:
+    Zigoto_Olho_Filho = Olhos_azuis
+
+print(f"{Zigoto_Olho_Filho}")
