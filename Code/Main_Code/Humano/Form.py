3e03fa943f17f1ed2fcd42824bb47e1085b590b9
JoaoIFSP <137102140+JoaoIFSP@users.noreply.github.com>
2024-12-11 15:56:35 -0300

    Arumei a praga do formulrio

--- Code/Main_Code/Humano/Form.py
+++ Code/Main_Code/Humano/Form.py
@@ -29,10 +29,10 @@ Pele = {
 
 Tipo_Cabelo = {
     
-    "Cacheado": 8,    # Dominante
+    "Crespo": 8,    # Dominante
     "Liso": 4,    # Dominante
-    "Grosso": 2,  # Recessivo
-    "Fino": 1    # Recessivo
+    "Cacheado": 2,  # Recessivo
+    "Ondulado": 1    # Recessivo
     
 }
 # Zigoto do Tipo de cabelo do papai e da mamãe
@@ -41,7 +41,7 @@ Nariz = {
     "Largo": 8,    # Dominante
     "Fino": 4,    # Dominante
     "Redondo": 2,  # Recessivo
-    "Reto": 1    # Recessivo
+    "Adunco": 1    # Recessivo
     
 }
 # Zigoto do Tipo de cabelo do papai e da mamãe
@@ -50,13 +50,13 @@ Altura = {
     "1,70 m": 8,    # Dominante
     "1,60 m": 4,    # Dominante
     "1,90 m": 2,  # Recessivo
-    "1,50 m": 1    # Recessivo
+    "1,40 m": 1    # Recessivo
     
 }
 
 # Olhos
 Zigoto_Olho_Mae = [Olhos["Castanho"], Olhos["Azul"]] # Opções:
-Zigoto_Olho_Pai = [Olhos["Preto"], Olhos["Verde"]] # Castanho(A) / Amarelo(A) / Azul(a) / Verde(a)
+Zigoto_Olho_Pai = [Olhos["Preto"], Olhos["Verde"]] # Castanho(A) / Preto(A) / Azul(a) / Verde(a)
 
 # Fim
 
@@ -66,8 +66,8 @@ Zigoto_Cabelo_Pai = [Cabelo["Preto"], Cabelo["Ruivo"]]  # Moreno(A) / Preto(A) /
 # Fim
 
 # Tipo de cabelo
-Zigoto_Tipo_Cabelo_Mae = [Tipo_Cabelo["Cacheado"], Tipo_Cabelo["Grosso"]] # Opções:
-Zigoto_Tipo_Cabelo_Pai = [Tipo_Cabelo["Liso"], Tipo_Cabelo["Fino"]]       # Cacheado(A) / Liso(A) / Grosso(a) / Fino(a)
+Zigoto_Tipo_Cabelo_Mae = [Tipo_Cabelo["Liso"], Tipo_Cabelo["Ondulado"]] # Opções:
+Zigoto_Tipo_Cabelo_Pai = [Tipo_Cabelo["Crespo"], Tipo_Cabelo["Cacheado"]]       # Liso(A) / Crespo(A) / Cacheado(a) / Ondulado(a)
 # Fim
 
 # Pele
@@ -77,12 +77,12 @@ Zigoto_Pele_Pai = [Pele["Branco"], Pele["Pardo"]]  # Negro(A) / Branco(A) / Amar
 
 # Nariz
 Zigoto_Nariz_Mae = [Nariz["Largo"], Nariz["Redondo"]] # Opções:
-Zigoto_Nariz_Pai = [Nariz["Fino"], Nariz["Reto"]]     # Largo(A) / Fino(A) / Redondo(a) / Reto(a)
+Zigoto_Nariz_Pai = [Nariz["Fino"], Nariz["Adunco"]]     # Largo(A) / Fino(A) / Redondo(a) / Adunco(a)
 # Fim
 
 # Altura
-Zigoto_Altura_Mae = [Altura["1,70 m"], Altura["1,50 m"]] # Opções:
-Zigoto_Altura_Pai = [Altura["1,60 m"], Altura["1,90 m"]] # 1,70 m (A) / 1,60 m (A) / 1,50 m(a) / 1,90 m(a)
+Zigoto_Altura_Mae = [Altura["1,70 m"], Altura["1,40 m"]] # Opções:
+Zigoto_Altura_Pai = [Altura["1,60 m"], Altura["1,90 m"]] # 1,70 m (A) / 1,60 m (A) / 1,40 m(a) / 1,90 m(a)
 # Fim
 
 
